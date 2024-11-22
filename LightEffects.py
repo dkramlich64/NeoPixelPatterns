64c68a2770cfbb7a4717cfbe6ef2735bc6d1200e
Dennis Kramlich <32776701+dkramlich64@users.noreply.github.com>
2024-11-22 15:16:08 -0500

    Update LightEffects.py
    Fixed snowfall and second_wipe with blue and white (DLK)

--- LightEffects.py
+++ LightEffects.py
@@ -1,93 +1,284 @@
+#imports
 import board
 from neopixel import NeoPixel
 import time
 import random
 
-np = NeoPixel(board.D2, 30, auto_write=False, brightness=0.3)
+np = NeoPixel(board.GP2,100,auto_write = False,brightness = .5, pixel_order="RGB")
 
-RED = 0
-GREEN = 1
-BLUE = 2
+#colors
+red = (230,15,0)
+green = (0,255,0)
+blue = (0,0,127)
+yellow = (255,50,0)
+cyan = (0,255,255)
+magenta = (255,0,255)
+pink = (255,53, 127)
+purple = (76,4,153)
+lightGreen = (70,247,70)
+orange = (250,75,5)
+darkGreen = (1, 40, 2)
+white = (255,255,255)
+gray = (100,100,100)
+black = (0,0,0)
 
-MAX = 255
+#variables
+lsts = [red, white,green]
+lst = [blue, white]
+y = 0
 
-color = [255, 0, 0]
-decrease = 0
-increase = 1
-count = 0
+#main functions
+#ONLY ADD FUNCTIONS THAT ARE CALLED IN THE MAIN LOOP
 
-def rainbow_static():
-    np.fill(color)
-    np.show()
-    time.sleep(0.03)
-    if count == 255:
-        decrease = (decrease + 1) % 3
-        increase = (increase + 1) % 3
-    else:
-        color[decrease] -= 1
-        color[increase] += 1
-    count = (count + 1) % 256
+def candyCane(color, color2):
+    """
+    simulates pattern of candy cane 
 
+    parameters:
+    color (tuple): the rgb sequence of a specific color
+    color2 (tuple): the rgb sequence of a specific color
 
-def fade_in(color):
-    red_step = color[RED] / MAX
-    green_step = color[GREEN] / MAX
-    blue_step = color[BLUE] / MAX
-    for i in range(MAX + 1):
-        new_color =  [int(i * red_step), int(i * green_step), int(i * blue_step)]
-        np.fill(new_color)
+    returns:
+    n/a
+
+    authors:
+    Jack and Daniel with assistance from Casey
+    """
+    count = 0
+    for i in range(np.n):
+        np.fill(color)
+        for i in range(np.n):
+            if (i + count) % 4 == 0:
+                np[i] = color2
+            if (i + count) % 4 == 1:
+                np[i] = color2
         np.show()
-        time.sleep(0.01)
+        time.sleep(.12)
+        count = (count + 1) % 3
 
-def fade_out(color):
-    red_step = color[RED] / MAX
-    green_step = color[GREEN] / MAX
-    blue_step = color[BLUE] / MAX
-    for i in range(MAX + 1):
-        new_color =  [color[RED] - int(i * red_step), color[GREEN] - int(i * green_step), color[BLUE] -int(i * blue_step)]
-        np.fill(new_color)
+def snowfall():
+    """
+    This fills up the entire strip with a chosen color
+    
+    parameters:
+    n/a
+    
+    returns:
+    n/a
+    
+    authors:
+    Oly & Jacobi
+    """
+    half = np.n// 2
+    for i in range(half):
+        np[half - 1 - i] = white
+        np[half + i] = white
+        time.sleep(0.2)
         np.show()
-        time.sleep(0.01)
+    count = 0
+#     for x in range(np.n):
+#         if x < np.n // 3 :
+#             for x in range(np.n):
+#                 if (x + count) % 3 == 0:
+#                     np[x] = (black)
+#         time.sleep(0.4)
+#         np.show()
+#         count = (count + x) % 3
+    for x in range(0, np.n, 3):
+        np[x] = black
+        np.show()
+        time.sleep(0.05)
+    for x in range(1, np.n, 3):
+        np[x] = black
+        np.show()
+        time.sleep(0.05)
+    for x in range(2, np.n, 3):
+        np[x] = black
+        np.show()
+        time.sleep(0.05)
 
-def color_transition(start, end):
-    red_mod, green_mod, blue_mod = 1, 1, 1
-    if start[RED] < end[RED]:
-        red_mod = -1
-    if start[GREEN] < end[GREEN]:
-        green_mod = -1
-    if start[BLUE] < end[BLUE]:
-        blue_mod = -1
+
+def dots(dotColor = darkGreen, black = (0,0,0), endColor = red, i = 0, b = (np.n - 1), timesleep = 0.1, count = 0):
+    """
+    two dots meet halfway in the center then once cross paths they fill the neopixels with the endColor as they reach the ends
+    
+    parameters:
+    dotColor (tuple): the rgb sequence of a specific color: set to darkGreen
+    black (tuple): the rgb sequence of a specific color: set to (0,0,0)
+    endColor (tuple): the rgb sequence of a specific color: set to red
+    i (int): set to 0
+    b (int): set to # of pixels - 1
+    timesleep (double): set to 0.03
+    count (int): set to 0
+    
+    returns:
+    n/a
+
+    authors:
+    Casey & Dana 
+    """
+    global y
+    for x in range(np.n):
+        np[i] = (dotColor)
+        np[(i - 1) % np.n] = black
+        i = (i + 1) % np.n
         
-    red_step = abs(start[RED] - end[RED]) / MAX
-    green_step = abs(start[GREEN] - end[GREEN]) / MAX
-    blue_step = abs(start[BLUE] - end[BLUE]) / MAX
-
-    for i in range(MAX + 1):
-        new_color =  [start[RED] - int(red_mod * i * red_step), start[GREEN] - int(green_mod * i * green_step), start[BLUE] -int(blue_mod * i * blue_step)]
-        print(new_color)
-        np.fill(new_color)
+        
+        np[b] = (dotColor)
+        np[(b + 1) % np.n] = black
+        b = (b - 1) % np.n
+        
         np.show()
-        time.sleep(0.05)
-   
+        time.sleep(timesleep) 
+        if(i == (np.n / 2)):
+            for l in range(np.n - 1):
+                if l % 2 == 0:
+                    np[l] = red
+                else:
+                    np[l] = green
+            time.sleep(2)
+            np.show()
 
-def sparkle(background, foreground):
-    for i in range(60):
-        np.fill(background)
-        for j in range(5):
-            np[random.randint(0, 29)] = foreground
+        if(y % 2 == 1):
+            black = (0,0,0)
+            dotColor = darkGreen
+            
+        
+    wipe(red)
+    for i in range(35):
+        sparkle(red, white, 0.015, 5)
+    np.fill(red)
+    time.sleep(0.2)
+    reverseWipe(green)
+    for i in range(35):
+        sparkle(green, white, 0.015, 5)
+    np.fill(green)
+    time.sleep(0.2)
+
+
+def lightWork():
+    """
+    This is the set of code that will show christmas lightwork on the leds. Fritz made this
+    
+    parameters:
+    n/a 
+
+    returns:
+    n/a
+
+    authors:
+    Fritz & Miking
+    """
+    chase1(lsts)
+    reverse_wipe(lst)
+    reverse_wipe(lsts)
+
+
+#inside functions
+#ONLY ADD FUNCTIONS THAT ARE CALLED IN OTHER FUNCTIONS HERE
+def wipe(color):
+    """
+    Changes the lights in a sequential order to a determined color 
+    
+    parameters:
+    Color (tuple): the rgb sequence of a specific color
+    
+    returns:
+    n/a
+
+    authors:
+    Dana 
+    """
+    for i in range(np.n):
+        np[i] = color
         np.show()
-        time.sleep(0.05)
+        time.sleep(0.06)
+        
+def reverseWipe(color):
+    """
+    Changes the lights in a  reverse sequential order to a determined color
+    
+    parameters:
+    Color (tuple): the rgb sequence of a specific color
+    
+    returns:
+    n/a
 
+    authors:
+    Dana 
+    """
+    for i in range(np.n - 1, -1, -1):
+        np[i] = color
+        np.show()
+        time.sleep(0.06)
+        
+def sparkle(baseColor, sparkleColor, speed, numSpark):
+    """
+    simulates a sparkling effect with a solid background
+    
+    parameters:
+    baseColor (tuple): the rgb sequence of a specific color
+    sparkleColor (tuple): the rgb sequence of a specific color
+    numSpark (int): number of sparkles shown
+    speed (double): the speed at which the sparkles show: set to 0.015
+    
+    returns:
+    n/a
+
+    authors:
+    Dana
+    """
+    np.fill(baseColor)                                                                                           
+    for i in range(numSpark):
+        y = random.randint(0, (np.n -1))
+        np[y] = sparkleColor
+        time.sleep(speed)
+    np.show()
+
+def chase1(colors):
+    """
+    chase1 is meant to make a line of colors down a row seemingly moving
+
+    parameters:
+    chase1(colors): a list containing tuples of rgb color sequences
+
+    returns:
+    n/a
+
+    authors:
+    Fritz & Miking
+    """
+    for i in range(np.n):
+        np[i] = colors[i%len(colors)]
+        np.show()
+        time.sleep(0.04)
+    
+def reverse_wipe(colors):
+    """
+    Reverse_wipe will be used to wipe chase1 depending on the list of colors
+
+    parameters:
+    colors(list): a list containing tuples of rgb color sequences
+
+    returns:
+    n/a
+
+    authors:
+    Fritz & Miking
+    """
+    for i in range(np.n):
+        i = i* -1
+        np[i] = random.choice(colors)
+        np.show()
+        time.sleep(0.04)
+
+
+#main loop
 while True:
-    color_transition([1, 255, 1], [255, 1, 255])
-    color_transition([255, 1, 255], [1, 255, 1])
-    '''        
-    fade_in([255, 0, 128])
-    fade_out([255, 0, 128])
-    fade_in([0, 0, 255])
-    fade_out([0, 0, 255])
-    fade_in([64, 255, 0])
-    fade_out([64, 255, 0])
-    sparkle([0, 0, 255], [255, 64, 0])
-    sparkle([127, 0, 0], [0, 127, 255])
-    '''
+    candyCane(white, red)
+    dots()
+    snowfall()
+    for i in range(3):
+        lightWork()
+    
+
