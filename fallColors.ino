4df7654a5e5debb5348ff315eca6c8832023357a
Dennis Kramlich <32776701+dkramlich64@users.noreply.github.com>
2025-11-06 11:07:43 -0500

    Update yellow color and add sweep effect to pixels
    Adjusted the yellow color value and modified the pixel color setting logic to include a sweep effect.

--- fallColors.ino
+++ fallColors.ino
@@ -9,7 +9,7 @@ Adafruit_NeoPixel strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 long red = strip.Color(127, 0, 0);
 long green = strip.Color(0, 127, 0);
 long blue = strip.Color(0, 0, 127);
-long yellow = strip.Color(127, 79, 0);
+long yellow = strip.Color(127, 63, 0);
 long orange = strip.Color(127, 31, 0);
 long cyan = strip.Color(0, 127, 127);
 long magenta = strip.Color(127, 0, 127);
@@ -20,6 +20,7 @@ long black = 0;
 
 long colors[] = {red, red, orange, orange, yellow, yellow};
 
+int sweep = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,7 +31,16 @@ void loop() {
   // put your main code here, to run repeatedly:
   for(int i = 0; i < NUMPIXELS; i++)
   {
-    strip.setPixelColor(i, (colors[i%6]));
+    strip.setPixelColor((i + sweep) % NUMPIXELS, (colors[i%6]));
+  }
+  long pixel;
+  for(int i = 0; i < NUMPIXELS / 4; i++) 
+  {
+    pixel = random(0, NUMPIXELS);
+    strip.setPixelColor(pixel, black);
   }
   strip.show();
+  delay(100);
+  sweep++;
+  sweep = sweep % 6;
 }
